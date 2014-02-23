044034397b0e8bf0d797d8a08b3b984cffe69104
John Ballantyne <johnb@ks4005973.ip-192-99-12.net>
2014-02-23 16:32:51 -0500

    Retab

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
-var https = require('https');
+var http = require('http');
 var soap = require('soap');
 var fs = require('fs');
 var myService = {
-	MyService: {
-	  MyPort: {
-	      MyFunction: function(args) {
-		  return {
-		      name: args.name
-		  };
-	      },
+    MyService: {
+        MyPort: {
+            MyFunction: function(args) {
+                return {
+                    name: args.name
+                };
+            },
 
-	      // This is how to define an asynchronous function.  
-	      MyAsyncFunction: function(args, callback) {
-		  // do some work
-		  callback({
-		      name: args.name
-		  })
-	      }
-	  }
-	}
-}
+            // This is how to define an asynchronous function.  
+            MyAsyncFunction: function(args, callback) {
+                // do some work
+                callback({
+                    name: args.name
+                })
+            }
+        }
+    }
+};
+
 var xml = fs.readFileSync('myservice.wsdl', 'utf8'),
-      server = https.createServer({
-    key: fs.readFileSync('server-key.pem'),
-    cert: fs.readFileSync('server-cert.pem')
-}
-,function(request,response) {
-          response.end("404: Not Found: "+request.url)
-      });
+server = http.createServer(function(request,response) {
+    response.end("404: Not Found: "+request.url)
+});
 server.listen(8000);
 soap.listen(server, '/wsdl', myService, xml);
