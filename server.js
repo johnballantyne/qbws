fb5302eee1ff4a71b301244f7d37f42d3f785129
John Ballantyne <johnballantyne@gmail.com>
2014-02-23 22:18:09 -0500

    Logs

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ soapServer.serverVersion = function() {
 };
 
 soapServer.log = function(type, data) {
-    //console.log("Log.type: ".yellow + type);
-    //console.log("Log.data: ".yellow + data);
+    console.log("Log.type: ".cyan + type);
+    console.log("Log.data: ".cyan + data);
 };
 
